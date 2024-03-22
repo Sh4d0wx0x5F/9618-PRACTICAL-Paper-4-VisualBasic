3165f9fc4abd165af79faf5bf92fa346031bb88c
Muhammad Arafat <97059953+Sh4d0wx0x5F@users.noreply.github.com>
2024-03-22 17:25:40 +0500

    Update q3.vb

--- 9618-41_ON2021/q3.vb
+++ 9618-41_ON2021/q3.vb
@@ -1,133 +1,151 @@
-' PART_A
-Dim ArrayNodes(19, 2) As Integer
-Dim RootPointer As Integer = -1
-Dim FreeNode As Integer = 0
+Module Main
+    ' PART A: Initializing array nodes, root pointer, and free node
+    Dim ArrayNodes(19, 2) As Integer
+    Dim RootPointer As Integer = -1
+    Dim FreeNode As Integer = 0
 
-' PART_B
-Sub AddNode(ArrayNodes, RootPointer, FreeNode)
-    Console.Write("Enter the Data: ")
-    Dim NodeData As Integer = Console.ReadLine()
-    If FreeNode <= 19 Then
-        ArrayNodes(FreeNode, 0) = -1
-        ArrayNodes(FreeNode, 1) = NodeData
-        ArrayNodes(FreeNode, 2) = -1
-        If RootPointer = -1 Then ' Add to start
-            RootPointer = 0
+    ' PART B: Function to add a node to the binary search tree
+    Function AddNode(ByRef ArrayNodes As Integer(,), ByRef RootPointer As Integer, ByRef FreeNode As Integer) As (Integer(,), Integer, Integer)
+        Dim NodeData As Integer
+        Console.Write("Enter the Data: ")
+        NodeData = Integer.Parse(Console.ReadLine())
+
+        If FreeNode <= 19 Then
+            ArrayNodes(FreeNode, 0) = -1
+            ArrayNodes(FreeNode, 1) = NodeData
+            ArrayNodes(FreeNode, 2) = -1
+
+            If RootPointer = -1 Then ' Add to start
+                RootPointer = 0
+            Else
+                Dim Placed As Boolean = False
+                Dim CurrentNode As Integer = RootPointer
+
+                While Not Placed
+                    If NodeData < ArrayNodes(CurrentNode, 1) Then
+                        If ArrayNodes(CurrentNode, 0) = -1 Then
+                            ArrayNodes(CurrentNode, 0) = FreeNode
+                            Placed = True
+                        Else
+                            CurrentNode = ArrayNodes(CurrentNode, 0)
+                        End If
+                    Else
+                        If ArrayNodes(CurrentNode, 2) = -1 Then
+                            ArrayNodes(CurrentNode, 2) = FreeNode
+                            Placed = True
+                        Else
+                            CurrentNode = ArrayNodes(CurrentNode, 2)
+                        End If
+                    End If
+                End While
+            End If
+
+            FreeNode = FreeNode + 1
         Else
-            Dim Placed As Boolean = False
-            Dim CurrentNode As Integer = RootPointer
-            While Not Placed
-                If NodeData < ArrayNodes(CurrentNode, 1) Then
-                    If ArrayNodes(CurrentNode, 0) = -1 Then
-                        ArrayNodes(CurrentNode, 0) = FreeNode
-                        Placed = True
-                    Else
-                        CurrentNode = ArrayNodes(CurrentNode, 0)
-                    End If
-                Else
-                    If ArrayNodes(CurrentNode, 2) = -1 Then
-                        ArrayNodes(CurrentNode, 2) = FreeNode
-                        Placed = True
-                    Else
-                        CurrentNode = ArrayNodes(CurrentNode, 2)
-                    End If
-                End If
-            End While
+            Console.WriteLine("Tree is full")
         End If
-        FreeNode = FreeNode + 1
-    Else
-        Console.WriteLine("Tree is full")
-    End If
-End Sub
 
-' PART_C
-Sub PrintAll(ArrayNodes)
-    For X As Integer = 0 To 19
-        Console.WriteLine(ArrayNodes(X, 0) & " " & ArrayNodes(X, 1) & " " & ArrayNodes(X, 2))
+        Return (ArrayNodes, RootPointer, FreeNode)
+    End Function
+
+    ' PART C: Function to print all nodes in the array
+    Sub PrintAll(ArrayNodes As Integer(,))
+        For X As Integer = 0 To 19
+            Console.WriteLine($"{ArrayNodes(X, 0)} {ArrayNodes(X, 1)} {ArrayNodes(X, 2)}")
+        Next
+    End Sub
+
+    ' PART D-1: Adding nodes to the binary search tree
+    For X As Integer = 0 To 9
+        Dim result = AddNode(ArrayNodes, RootPointer, FreeNode)
+        ArrayNodes = result.Item1
+        RootPointer = result.Item2
+        FreeNode = result.Item3
     Next
-End Sub
 
-' PART_D-1
-For X As Integer = 0 To 9
-    AddNode(ArrayNodes, RootPointer, FreeNode)
-Next
-PrintAll(ArrayNodes)
+    PrintAll(ArrayNodes)
 
-' PART_D-2
+    ' PART D-2: Provided code as is
+    '''
+    Enter the Data10
+    Enter the Data5
+    Enter the Data15
+    Enter the Data8
+    Enter the Data12
+    Enter the Data6
+    Enter the Data20
+    Enter the Data11
+    Enter the Data9
+    Enter the Data4
+    1   10   2
+    9   5   3
+    4   15   6
+    5   8   8
+    7   12   -1
+    -1   6   -1
+    -1   20   -1
+    -1   11   -1
+    -1   9   -1
+    -1   4   -1
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    '''
 
-' Output:
-' 1 10 2
-' 9 5 3
-' 4 15 6
-' 5 8 8
-' 7 12 -1
-' -1 6 -1
-' -1 20 -1
-' -1 11 -1
-' -1 9 -1
-' -1 4 -1
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
-' 0 0 0
+    ' PART E-1: Function to perform in-order traversal of the binary search tree
+    Sub InOrder(ArrayNodes As Integer(,), RootNode As Integer)
+        If ArrayNodes(RootNode, 0) <> -1 Then
+            InOrder(ArrayNodes, ArrayNodes(RootNode, 0))
+        End If
 
-' PART_E-1
-Sub InOrder(ArrayNodes, RootNode)
-    If ArrayNodes(RootNode, 0) <> -1 Then
-        InOrder(ArrayNodes, ArrayNodes(RootNode, 0))
         Console.WriteLine(ArrayNodes(RootNode, 1))
+
         If ArrayNodes(RootNode, 2) <> -1 Then
             InOrder(ArrayNodes, ArrayNodes(RootNode, 2))
         End If
-    End If
-End Sub
+    End Sub
 
-' PART_E-2
+    ' PART E-2: Provided code as is
+    '''
+    Enter the Data: 10
+    Enter the Data: 5
+    Enter the Data: 15
+    Enter the Data: 8
+    Enter the Data: 12
+    Enter the Data: 6
+    Enter the Data: 20
+    Enter the Data: 11
+    Enter the Data: 9
+    Enter the Data: 4
+    1   10   2
+    9   5   3
+    4   15   6
+    5   8   8
+    7   12   -1
+    -1   6   -1
+    -1   20   -1
+    -1   11   -1
+    -1   9   -1
+    -1   4   -1
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    0   0   0
+    '''
 
-' Output:
-' Enter the Data: 10
-' Enter the Data: 5
-' Enter the Data: 15
-' Enter the Data: 8
-' Enter the Data: 12
-' Enter the Data: 6
-' Enter the Data: 20
-' Enter the Data: 11
-' Enter the Data: 9
-' Enter the Data: 4
-'1   10   2
-'9   5   3
-'4   15   6
-'5   8   8
-'7   12   -1
-'-1   6   -1
-'-1   20   -1
-'-1   11   -1
-'-1   9   -1
-'-1   4   -1
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-'0   0   0
-' 4
-' 5
-' 6
-' 8
-' 9
-' 10
-' 11
-' 12
-' 15
-' 20
+    'Console.WriteLine("In-Order Traversal:")'
+    'InOrder(ArrayNodes, RootPointer)'
+End Module
